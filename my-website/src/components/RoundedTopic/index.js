a02a43627bcdff34a35275546b4ba644f80b9236
Fábio Martins <fabio.m@ua.pt>
2022-08-30 17:56:40 +0100

    added transition and formatted code

--- my-website/src/components/RoundedTopic/index.js
+++ my-website/src/components/RoundedTopic/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const RoundedTopic = (props) => {
-    const className= "bg-white aspect-square text-dark-primary flex items-center justify-center h-1/6 " + props.className
+    const className = "bg-white aspect-square text-dark-primary flex items-center justify-center h-1/3 hover:scale-110 transition-all ease-linear " + props.className
     return (
-        <div className={className} style={props.style} onClick={props.onClick}>
+        <div
+            id={props.id}
+            className={className}
+            style={props.style}
+            onClick={props.onClick}>
             <p className="text-xl font-medium">{props.title}</p>
         </div>
     )
