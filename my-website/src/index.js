b6a5fb6a6eb0916c902da81528fb841de6bce50a
Fábio Martins <fabio.m@ua.pt>
2022-09-01 18:14:21 +0100

    removed header

--- my-website/src/index.js
+++ my-website/src/index.js
@@ -4,12 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Header></Header>
     <App />
   </BrowserRouter>
 );
