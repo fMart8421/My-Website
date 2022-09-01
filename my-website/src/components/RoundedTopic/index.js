27dbc9abbd9b1e2dbc3b7018dd7d3130a7baca12
Fábio Martins <fabio.m@ua.pt>
2022-09-01 18:14:46 +0100

    added links

--- my-website/src/components/RoundedTopic/index.js
+++ my-website/src/components/RoundedTopic/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const RoundedTopic = (props) => {
     const className = "bg-white aspect-square text-dark-primary flex items-center justify-center h-1/3 hover:scale-110 transition-all ease-linear " + props.className
+    const path = `/${props.title.toLowerCase()}`;
+
     return (
-        <div
-            id={props.id}
-            className={className}
-            style={props.style}
-            onClick={props.onClick}>
-            <p className="text-xl font-medium">{props.title}</p>
-        </div>
+        <Link to={path} className="text-xl font-medium">
+            <p
+                id={props.id}
+                className={className}
+                style={props.style}
+                onClick={props.onClick}>
+                    {props.title}
+            </p>
+        </Link>
     )
 }
 
