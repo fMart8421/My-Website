c0127d022f0eebd094dd3f51a5571baaecdd94ce
Fábio Martins <fabio.m@ua.pt>
2022-09-02 15:30:57 +0100

    created contact component

---
+++ my-website/src/components/Contact/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Contact = (props) => {
+    return (
+        <div className="relative flex items-center ml-8 text-xl flex-grow-0">
+            <img src={props.image} alt={props.alt} className=""></img>
+            {props.children}
+            <div onClick={props.onClick}
+                className="absolute w-full h-full bg-dark-primary/95 flex justify-center items-center transition-opacity opacity-0 hover:opacity-100 duration-500"
+            >
+                <p>{props.hoverContent}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
