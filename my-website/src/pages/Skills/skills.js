9306da9d765ea53839e2d7be2fa2ae46ac64e028
Fábio Martins <fabio.m@ua.pt>
2022-09-05 21:21:41 +0100

    skills list

---
+++ my-website/src/pages/Skills/skills.js
@@ -0,0 +1,52 @@
+export const skillLevels = [
+    {
+        "name":"ReactJs",
+        "level": 4,
+        "icon": "/svg/skills/react-icon.svg"
+    },
+    {
+        "name":"TailwindCSS",
+        "level": 4,
+        "icon": "/svg/skills/tailwindcss-icon.svg"
+    },
+    {
+        "name":"Java",
+        "level": 4,
+        "icon": "/svg/skills/java-icon.svg"
+    },
+    {
+        "name":"JavaScript",
+        "level": 3,
+        "icon": "/svg/skills/javascript-icon.svg"
+    },
+    {
+        "name":"HTML",
+        "level": 3,
+        "icon": "/svg/skills/html-icon.svg"
+    },
+    {
+        "name":"CSS",
+        "level": 3,
+        "icon": "/svg/skills/css-icon.svg"
+    },
+    {
+        "name":"Vue.js",
+        "level": 3,
+        "icon": "/svg/skills/vuejs-icon.svg"
+    },
+    {
+        "name":"Python",
+        "level": 3,
+        "icon": "/svg/skills/python-icon.svg"
+    },
+    {
+        "name":"Angular",
+        "level": 0,
+        "icon": "/svg/skills/angular-icon.svg"
+    },
+    {
+        "name":"NodeJS",
+        "level": 0,
+        "icon": "/svg/skills/nodejs-icon.svg"
+    }
+]
