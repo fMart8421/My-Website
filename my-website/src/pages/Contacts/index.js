c99e298007ef5f8f7586058764ffc7ecf6e6bc28
Fábio Martins <fabio.m@ua.pt>
2022-09-05 21:22:14 +0100

    changed svg path

--- my-website/src/pages/Contacts/index.js
+++ my-website/src/pages/Contacts/index.js
@@ -2,9 +2,6 @@ import React from 'react'
 
 import Contact from '../../components/Contact'
 
-import email from "../../utilities/svg/email.svg"
-import linkedin from "../../utilities/svg/linkedin.svg"
-import instagram from "../../utilities/svg/instagram.svg"
 import { openWindow } from '../../utilities/functions/utilityFunctions'
 
 const Contacts = () => {
@@ -15,13 +12,13 @@ const Contacts = () => {
         <p className="text-6xl text-justify">You want to contact me? Here's how!</p>
       </div>
       <div className="flex flex-col gap-16 mt-16">
-        <Contact onClick={()=>{openWindow("email")}} alt="Email" image={email} hoverContent="fabio.martins.work@gmail.com">
+        <Contact onClick={()=>{openWindow("email")}} alt="Email" image={"/svg/contacts/email.svg"} hoverContent="fabio.martins.work@gmail.com">
         <p className="text-3xl ml-8">Send me an <span className="text-purple-500">email</span> ...</p>
         </Contact>
-        <Contact onClick={()=>{openWindow("linkedin")}} alt="LinkedIn" image={linkedin} hoverContent="Fábio Martins">
+        <Contact onClick={()=>{openWindow("linkedin")}} alt="LinkedIn" image={"/svg/contacts/email.svg"} hoverContent="Fábio Martins">
         <p className="text-3xl ml-8"><span className="text-purple-500">Connect</span> with me ...</p>
         </Contact>
-        <Contact onClick={()=>{openWindow("instagram")}} alt="Instagram" image={instagram} hoverContent="fabio_mart0">
+        <Contact onClick={()=>{openWindow("instagram")}} alt="Instagram" image={"/svg/contacts/email.svg"} hoverContent="fabio_mart0">
         <p className="text-3xl ml-8">Or <span className="text-purple-500">stalk</span> me if you wish</p>
         </Contact>
       </div>
