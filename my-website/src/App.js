ad84d801afe97f3c9dbb2993edd9ab42ba7c9e1e
Fábio Martins <fabio.m@ua.pt>
2022-09-07 21:15:03 +0100

    added about and hobbies page

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -3,13 +3,15 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store/store';
 
+import Header from './components/Header';
+
 import Welcome from './pages/Welcome';
 import Home from './pages/Home';
 import GitHub from './pages/GitHub';
-
-import Header from './components/Header';
 import Contacts from './pages/Contacts';
 import Skills from './pages/Skills';
+import About from './pages/About';
+import Hobbies from './pages/Hobbies';
 
 function App() {
 
@@ -30,6 +32,8 @@ function App() {
         <Route exact path="/github" element={<GitHub></GitHub>} />
         <Route exact path="/contacts" element={<Contacts></Contacts>} />
         <Route exact path="/skills" element={<Skills></Skills>} />
+        <Route exact path="/hobbies" element={<Hobbies></Hobbies>} />
+        <Route exact path="/about" element={<About></About>} />
       </Routes>
 
     </Provider>
---
+++ my-website/src/pages/About/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>Welcome to the "hidden" page!</div>
+  )
+}
+
+export default About
