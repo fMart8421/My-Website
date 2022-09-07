a41336b64bc74f1c062425d41fcd2fbc04005b40
Fábio Martins <fabio.m@ua.pt>
2022-09-07 21:15:42 +0100

    changed to iterate over props

--- my-website/src/components/GithubProject/index.js
+++ my-website/src/components/GithubProject/index.js
@@ -1,31 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react';
 
 const GithubProject = (props) => {
 
-    const [languages, setLanguages] = useState([]);
-
-    useEffect(() => {
-        if (props.languages) {
-            let _languages = [];
-            for (const language of props.languages) {
-                _languages.push(language);
-            }
-            setLanguages(_languages);
-        }
-    }, [props.languages])
 
     return (
-        <div className="bg-white text-dark-primary container flex flex-col flex-wrap rounded"
+        <div className="bg-white text-dark-primary container flex flex-col justify-between flex-wrap rounded"
         
         >
-            <div className="bg-dark-primary mt-4 flex justify-center mx-[10%] mb-2 rounded">
+            <div className="bg-dark-primary mt-4 flex justify-center mx-[10%] mb-2 rounded cursor-pointer" onClick={props.redirectTo}>
                 <p className="py-2 text-xl text-white ">{props.title}</p>
             </div>
             <p className="text-justify text-lg mt-2">{props.description}</p>
             <div className="mt-10 flex justify-between flex-wrap mb-2">
-                {languages.map((language, key)=>{
-                    return(
-                        <p key={key}>{language}</p>
+                {props.languages.map((language, key) => {
+                    return (
+                        <p className="mx-1" key={key}>{language}</p>
                     )
                 })}
             </div>
