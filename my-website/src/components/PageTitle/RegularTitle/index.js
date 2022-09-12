a2b99a7d56ba2e37e40f3b96b0caee8a706b1e0f
Fábio Martins <fabio.m@ua.pt>
2022-09-12 14:55:22 +0100

    removed unnecessary styling

--- my-website/src/components/PageTitle/RegularTitle/index.js
+++ my-website/src/components/PageTitle/RegularTitle/index.js
@@ -4,7 +4,6 @@ const RegularTitle = (props) => {
   return (
     <div
             className={props.className}
-            style={{ top: props.top, width: props.width }}
         >
             <p className="text-3xl font-semibold pt-4 pb-4">{props.title}</p>
         </div>
