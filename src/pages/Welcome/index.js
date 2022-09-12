fe2a7e69ea7abddb7b1048f0404e8209a7363603
Fábio Martins <fabio.m@ua.pt>
2022-09-12 21:24:23 +0100

    changed welcome animation

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,13 +1,19 @@
 import React, {  } from 'react'
 import { useNavigate } from 'react-router-dom'
+
 import Name from '../../components/Name'
+import {sleep} from '../../utilities/functions/utilityFunctions'
 
 const Welcome = () => {
 
     const navigate = useNavigate();
 
     const triggerChangePageAnimation = async () => {
-        document.getElementById("welcomePage").classList.add("blurr-fade-out");
+        document.getElementById("welcomePage_greySide").classList.add("width-shrink");
+        await sleep(800);
+        document.getElementById("welcomePage").classList.add("width-shrink");
+        await sleep(800);
+        navigate("/home");
     }
 
 
@@ -15,16 +21,19 @@ const Welcome = () => {
         <div 
         id="welcomePage"
         className="grid grid-cols-5 flex-grow transition "
-        onAnimationEnd={()=>{navigate("/home")}} 
         >
-            <div className=" col-span-2 flex flex-col bg-black pt-36 pl-[15%] pr-[15%]">
+            <div 
+            id="welcomePage_blackSide"
+            className=" col-span-2 flex flex-col bg-black pt-36 pl-[15%] pr-[15%]"
+            >
                 <p className="text-[4rem]">Hello there!</p>
                 <p className="text-[4rem] mt-4">I am</p>
                 <Name className="text-[5rem] pt-14 ml-16 bg-black grid grid-cols-1 transition" />
-
-
             </div>
-            <div className="col-span-3 pt-32 pl-[15%] pr-[15%] flex flex-col">
+            <div 
+            id="welcomePage_greySide"
+            className="col-span-3 pt-32 pl-[15%] pr-[15%] flex flex-col"
+            >
                 <p className="text-[3rem]">Are you here to learn more about me? Well then...</p>
                 <div
                     className="self-center flex flex-col items-center mt-16 max-w-max cursor-pointer"
