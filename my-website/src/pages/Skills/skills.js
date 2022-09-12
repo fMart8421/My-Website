4afbfb4eaf1d5533ad0911119d2bb4f41faccbf0
Fábio Martins <fabio.m@ua.pt>
2022-09-12 14:46:39 +0100

    added id and new skill

--- my-website/src/pages/Skills/skills.js
+++ my-website/src/pages/Skills/skills.js
@@ -1,50 +1,72 @@
 export const skillLevels = [
     {
+        "id":1,
         "name":"ReactJs",
         "level": 4,
         "icon": "/svg/skills/react-icon.svg"
     },
     {
+        "id":2,
         "name":"TailwindCSS",
         "level": 4,
         "icon": "/svg/skills/tailwindcss-icon.svg"
     },
     {
+        "id":3,
         "name":"Java",
         "level": 4,
         "icon": "/svg/skills/java-icon.svg"
     },
     {
+        "id":4,
         "name":"JavaScript",
         "level": 3,
         "icon": "/svg/skills/javascript-icon.svg"
     },
     {
+        "id":5,
         "name":"HTML",
         "level": 3,
         "icon": "/svg/skills/html-icon.svg"
     },
     {
+        "id":6,
         "name":"CSS",
         "level": 3,
         "icon": "/svg/skills/css-icon.svg"
     },
     {
+        "id":7,
         "name":"Vue.js",
         "level": 3,
         "icon": "/svg/skills/vuejs-icon.svg"
     },
     {
+        "id":8,
         "name":"Python",
         "level": 3,
         "icon": "/svg/skills/python-icon.svg"
     },
     {
+        "id":9,
+        "name":"TypeScript",
+        "level": 2,
+        "icon": "/svg/skills/typescript-icon.svg"
+    },
+    {
+        "id":10,
+        "name":"Flutter",
+        "level": 2,
+        "icon": "/svg/skills/flutter-icon.svg"
+    },
+    {
+        "id":11,
         "name":"Angular",
         "level": 0,
         "icon": "/svg/skills/angular-icon.svg"
     },
     {
+        "id":12,
         "name":"NodeJS",
         "level": 0,
         "icon": "/svg/skills/nodejs-icon.svg"
