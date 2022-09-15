9935afdfe5d5fae51f8135c6618bb4800af0e122
Fábio Martins <fabio.m@ua.pt>
2022-09-15 16:32:24 +0100

    paths file

---
+++ src/pages/Path/paths.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styles from './index.module.css'
+
+
+export const paths = [
+    {
+        id:"path1",
+        title:"AHBVCS",
+        location:"Associação Humanitária dos Bombeiros Voluntários de Carregal do Sal",
+        logo:"/svg/path/ahbvcs-logo.svg",
+        mission:"Volunteer Firefighter",
+        details:"In 2017, the fires were devastating in Portugal. A lot of forests were burned down and even houses were destroyed. Where I lived, Papízios, an old house caught fire, right in the middle of the population, and I found myself trying to fight the fire in the middle of the night, in order to save my parents’ farm and house. The next year, the local firefighters went to my school and explained their job to the students. That’s when I thought I wanted to help. Throughout my stay in the firefighters, I learned how to perform First Aid for medical emergencies, vehicle extrication and how to extinguish fires. Everyone had their jobs there. Mine, for most of my stay, was connected with Telecomunications and, after getting my license, driving ambulances in emergencies.",
+        pathDetails:{
+            id:"path1_dashedPath",
+            path:<path d="M2 7.71832C155.861 -15.8212 371.402 41.8382 484 27.7521" stroke="white" strokeWidth="4" strokeLinecap="round" />,
+            svgClassName:`col-span-4 ${styles["continuous-offset"]}`,
+            viewWidth:"486",
+            viewHeight:"32"
+        }
+    },
+    {
+        id:"path2",
+        title:"NEI AAUAv",
+        location:"Núcleo de Estudantes de Informática AAUAv",
+        logo:"/svg/path/nei-logo.svg",
+        mission:"External Colaborator",
+        details:"In 2017, the fires were devastating in Portugal. A lot of forests were burned down and even houses were destroyed. Where I lived, Papízios, an old house caught fire, right in the middle of the population, and I found myself trying to fight the fire in the middle of the night, in order to save my parents’ farm and house. The next year, the local firefighters went to my school and explained their job to the students. That’s when I thought I wanted to help. Throughout my stay in the firefighters, I learned how to perform First Aid for medical emergencies, vehicle extrication and how to extinguish fires. Everyone had their jobs there. Mine, for most of my stay, was connected with Telecomunications and, after getting my license, driving ambulances in emergencies.",
+        pathDetails:{
+            id:"path2_dashedPath",
+            path:<path d="M2 5.46667C166.452 -8.54772 370 25.3385 492 25.3386" stroke="white" stroke-width="4" strokeLinecap="round"/>,
+            svgClassName:`col-span-4 ${styles["continuous-offset"]}`,
+            viewWidth:"494",
+            viewHeight:"28"
+        }
+    },
+    {
+        id:"path3",
+        title:"NEI AAUAv",
+        location:"Núcleo de Estudantes de Informática AAUAv",
+        logo:"/svg/path/nei-logo.svg",
+        mission:"Member of the Academic Section",
+        details:"In 2017, the fires were devastating in Portugal. A lot of forests were burned down and even houses were destroyed. Where I lived, Papízios, an old house caught fire, right in the middle of the population, and I found myself trying to fight the fire in the middle of the night, in order to save my parents’ farm and house. The next year, the local firefighters went to my school and explained their job to the students. That’s when I thought I wanted to help. Throughout my stay in the firefighters, I learned how to perform First Aid for medical emergencies, vehicle extrication and how to extinguish fires. Everyone had their jobs there. Mine, for most of my stay, was connected with Telecomunications and, after getting my license, driving ambulances in emergencies.",
+        pathDetails:{
+            id:"path3_dashedPath",
+            path:<path d="M2.5 180C2.5 144.5 206 -14.5 651.5 64.5C974 147.5 1322 48.5 1298 2" stroke="white" strokeWidth="4" strokeLinecap="round"/>,
+            svgClassName:`col-span-11 pb-4 px-8 ${styles["continuous-offset-reverse"]}`,
+            viewWidth:"1302",
+            viewHeight:"182"
+        }
+    },
+    {
+        id:"path4",
+        title:"Unknown",
+        location:"Unknown",
+        logo:"/svg/path/unknown.svg",
+        mission:"Unknown",
+        details:"The future is an unknown variable, always defined by the paths we've taken so far. A simple decision that is taken today might influence your whole day tomorrow. Butterfly effect my friend, butterfly effect.",
+        pathDetails:{
+            id:"path4_dashedPath",
+            path:<path d="M2.5 15C108.5 58.5 281.1 -6.22205 404 2.88639" stroke="white" stroke-width="4" strokeLinecap="round"/>,
+            svgClassName:`col-span-4 ${styles["continuous-offset"]}`,
+            viewWidth:"406",
+            viewHeight:"33"
+        }
+    },
+]
