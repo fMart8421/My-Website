f8c1150141da6625ab68127f147edadd342b412b
Fábio Martins <fabio.m@ua.pt>
2022-09-21 19:12:01 +0100

    added X icon back

--- src/components/MenuTitle/index.js
+++ src/components/MenuTitle/index.js
@@ -5,6 +5,7 @@ const MenuTitle = (props) => {
         <div className="w-full lg:h-[75px] border-b-4 border-white flex justify-between items-center py-1 lg:py-4 px-2 lg:px-10">
             <img alt="" src={props.menuIcon} className=" h-[25px] lg:h-[50px]"></img>
             <p className="text-lg md:text-xl lg:text-2xl px-8">{props.title}</p>
+            <img alt="Close Icon" src={"/svg/close-icon.svg"} className=" h-[25px] lg:h-[50px]"></img>
         </div>
     )
 }
