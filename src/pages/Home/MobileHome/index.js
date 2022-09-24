025b1e68fffe2ab77713fce04c75604b51ad279f
Fábio Martins <fabio.m@ua.pt>
2022-09-24 11:53:12 +0100

    reverted to old home

--- src/pages/Home/MobileHome/index.js
+++ src/pages/Home/MobileHome/index.js
@@ -1,174 +1,37 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { Link } from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import {Link} from 'react-router-dom'
 import { topics } from '../topics'
-import { sleep } from '../../../utilities/functions/utilityFunctions'
+
 import styles from './index.module.css'
 
 const MobileHome = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  let start = 0;
-  const [swipe, setSwipe] = useState(0);
-  const [displayedCards, setDisplayedCards] = useState([]);
-  const previousRef = useRef(null);
-  const mainRef = useRef(null);
-  const nextRef = useRef(null);
-
-
-
-
-  const getRoundedSelectors = () => {
-    return topics.map((_, index) => {
-      let className = "";
-      if (index === currentIndex) {
-        className = "aspect-square h-2 md:h-4 bg-white first:mt-0 last:mb-0 my-6 rounded-full "
-      } else {
-        className = "aspect-square h-2 md:h-4 bg-white/50 first:mt-0 last:mb-0 my-6 rounded-full "
-      }
-      return (
-        <div key={`selector-${index}`} className={className}>
-        </div>
-      )
-    })
-  }
-  const onTouchHandler = (event) => {
-    start = event.touches[0].pageY;
-  }
-  const onTouchStopHandler = (event) => {
-    setSwipe(event.changedTouches[0].pageY - start);
-  }
-
+  const [squaredTopics, setSquaredTopics] = useState(<></>)
   useEffect(() => {
-    window.addEventListener("touchstart", onTouchHandler)
-    window.addEventListener("touchend", onTouchStopHandler)
+      setSquaredTopics(topics.map((topic, index)=>{
 
-    setDisplayedCards(
-      [
-        <Link
-          ref={previousRef}
-          className={`bg-white rounded-md text-dark-primary text-center md:text-2xl w-3/4 py-8 md:py-14 ${styles["previous-item"]} ${styles["rotate-previous-in"]}`}
-          key={topics[topics.length - 1].id}
-          to={topics[topics.length - 1].path}>{topics[topics.length - 1].name}</Link>,
-        <Link
-          ref={mainRef}
-          className={`bg-white rounded-md text-dark-primary text-center md:text-2xl w-3/4 py-8 md:py-14 ${styles["fade-in"]}`}
-          key={topics[0].id}
-          to={topics[0].path}>{topics[0].name}</Link>,
-        <Link
-          ref={nextRef}
-          className={`bg-white rounded-md text-dark-primary text-center md:text-2xl w-3/4 py-8 md:py-14 ${styles["next-item"]} ${styles["rotate-next-in"]}`}
-          key={topics[1].id}
-          to={topics[1].path}>{topics[1].name}</Link>,
-      ]
-    )
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  useEffect(() => {
-    const timeoutID = setTimeout(() => {
-      if (swipe < 0) {
-        previousRef.current.classList.remove(styles["rotate-previous-in"]);
-        nextRef.current.classList.remove(styles["rotate-next-in"]);
-        previousRef.current.classList.add(styles["rotate-previous-out"]);
-        mainRef.current.classList.add(styles["rotate-main-to-previous"]);
-        nextRef.current.classList.add(styles["rotate-next-to-main"]);
-        setCurrentIndex((prevState) => {
-          return incremented(prevState)
-        })
-        getNewDisplayedCards(incremented(currentIndex) , 0);
-      } else if (swipe > 0) {
-        previousRef.current.classList.remove(styles["rotate-previous-in"]);
-        nextRef.current.classList.remove(styles["rotate-next-in"]);
-        previousRef.current.classList.add(styles["rotate-previous-to-main"]);
-        mainRef.current.classList.add(styles["rotate-main-to-next"]);
-        nextRef.current.classList.add(styles["rotate-next-out"]);
-        getNewDisplayedCards(decremented(currentIndex), 1);
-        setCurrentIndex((prevState) => {
-          return decremented(prevState)
-        })
-      }
-    }, 200)
-
-    return (() => {
-      clearTimeout(timeoutID);
-    })
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [swipe])
-
-  const incremented = n => {
-    return n === topics.length - 1 ? 0 : n + 1
-  }
-
-  const decremented = n =>{
-   return n === 0 ? topics.length - 1 : n - 1
-  }
-
-
-
-  const getNewDisplayedCards = async (_index, direction) => {
-
-    let currentCard = _index, previousCard = _index - 1, nextCard = _index + 1;
-    if (_index === 0) {
-      previousCard = topics.length - 1
-    } else if (_index === topics.length - 1) {
-      nextCard = 0;
-    }
-
-    await sleep(1700);
-    if (direction) {
-      setDisplayedCards(
-        [
+        return(
           <Link
-            ref={previousRef}
-            className={`bg-white rounded-md text-dark-primary text-center md:text-2xl w-3/4 py-8 md:py-14 ${styles["previous-item"]} ${styles["rotate-previous-in"]}`}
-            key={topics[previousCard].id}
-            to={topics[previousCard].path}>{topics[previousCard].name}</Link>,
-          <Link
-            ref={mainRef}
-            className={`bg-white rounded-md text-dark-primary text-center md:text-2xl w-3/4 py-8 md:py-14`}
-            key={topics[currentCard].id}
-            to={topics[currentCard].path}>{topics[currentCard].name}</Link>,
-          <Link
-            ref={nextRef}
-            className={`bg-white rounded-md text-dark-primary text-center md:text-2xl w-3/4 py-8 md:py-14 ${styles["next-item"]}`}
-            key={topics[nextCard].id}
-            to={topics[nextCard].path}>{topics[nextCard].name}</Link>,
-        ]
-      )
-    } else {
-      setDisplayedCards(
-        [
-          <Link
-            ref={previousRef}
-            className={`bg-white rounded-md text-dark-primary text-center md:text-2xl w-3/4 py-8 md:py-14 ${styles["previous-item"]}`}
-            key={topics[previousCard].id}
-            to={topics[previousCard].path}>{topics[previousCard].name}</Link>,
-          <Link
-            ref={mainRef}
-            className={`bg-white rounded-md text-dark-primary text-center md:text-2xl w-3/4 py-8 md:py-14`}
-            key={topics[currentCard].id}
-            to={topics[currentCard].path}>{topics[currentCard].name}</Link>,
-          <Link
-            ref={nextRef}
-            className={`bg-white rounded-md text-dark-primary text-center md:text-2xl w-3/4 py-8 md:py-14 ${styles["next-item"]} ${styles["rotate-next-in"]}`}
-            key={topics[nextCard].id}
-            to={topics[nextCard].path}>{topics[nextCard].name}</Link>,
-        ]
-      )
-    }
+          key={topic.id}
+          className={`font-medium bg-white odd:rounded-tl-full odd:rounded-br-full odd:origin-top-right even:rounded-bl-full even:rounded-tr-full even:origin-top-left text-dark-primary py-4 flex items-center justify-center hover:scale-110 transition-all ease-out duration-500 relative m-2 group fade-in`}
+          style={{animationDelay:`${index*200}ms`}}
+          to={topic.path}>
+          <p
+              className="fade-in z-10 md:text-lg lg:text-xl"
+          >
 
-  }
+              {topic.name}
+          </p>
+      </Link>
+        )
+      }))
+  },[])
+
   return (
-    <div className="relative flex-grow flex-shrink basis-0 flex justify-center items-center py-10 px-[10%]">
-
-      <div
-        className={`absolute top-1/2 right-4 -translate-y-1/2`}
-      >
-        {getRoundedSelectors()}
-      </div>
-      <div className={`w-full flex flex-col items-center relative`} >
-        {displayedCards}
-      </div>
-
+    <div className="relative flex-grow flex-shrink basis-0 grid grid-rows-3 grid-cols-2 py-10 px-[10%]">
+        {squaredTopics}
+        <div 
+        className={`absolute top-0 left-1/2 h-full w-4 bg-white -translate-x-1/2 open-scroll ${styles["slide-down"]}`}
+        ></div>
     </div>
   )
 }
