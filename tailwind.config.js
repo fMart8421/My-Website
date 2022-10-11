0028d6da7a3c5ef661cd2bb3d40d5e406348bc49
Fábio Martins <fabio.m@ua.pt>
2022-10-11 18:30:00 +0100

    fixed config bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,15 @@ module.exports = {
         DEFAULT: '10%',
       }
     },
-    cursor:{
-      pointer:"url(../public/svg/cursor/cursor-pointer.svg) 22 22, pointer"
-    },
+    
     extend: {
       colors:{
         'dark-primary':'#111'
-      }
+      },
+      cursor:{
+        default:"url(../public/svg/cursor/cursor.svg), auto;",
+        pointer:"url(../public/svg/cursor/cursor-pointer.svg) 22 22, pointer"
+      },
 
     },
   },
